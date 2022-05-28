d3fd894b367c302c91213e2ef4f0037931a7b63d
Philix27 <philixbob@gmail.com>
2022-05-28 12:20:27 +0100

    deleted every package and files, started from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,49 @@
 import 'dart:io';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:pharmahub/core/app_widget.dart';
-import 'package:pharmahub/models/article.dart';
-import 'package:pharmahub/models/user.dart';
+// import 'package:flutter/services.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  // await GetStorage.init('MyStorage');
-  final Directory appDocumentDirectory =
-      await getApplicationDocumentsDirectory();
+//   WidgetsFlutterBinding.ensureInitialized();
+//   // await Firebase.initializeApp();
+//   final Directory appDocumentDirectory =
+//       await getApplicationDocumentsDirectory();
 
-//? Hive
-  await Hive.initFlutter();
-  Hive.registerAdapter(ArticleAdapter());
-  Hive.registerAdapter(AppUserAdapter());
-  await Hive.openBox<Article>("bookmarks");
-  await Hive.openBox<String>("completedArticles");
-  await Hive.openBox<AppUser>("userInfo");
+// //? SystemChrome
+//   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//     statusBarColor: Colors.grey[200],
+//     statusBarIconBrightness: Brightness.light,
+//     statusBarBrightness: Brightness.light,
+//   ));
 
-//? SystemChrome
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.grey[200],
-    // statusBarColor: Color(0xFF087040),
-    // statusBarIconBrightness: Brightness.dark,
+//   SystemChrome.setEnabledSystemUIOverlays(
+//       [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+//   SystemChrome.setPreferredOrientations([
+//     DeviceOrientation.portraitUp,
+//     DeviceOrientation.portraitDown,
+//   ]);
 
-    statusBarIconBrightness: Brightness.light,
-    statusBarBrightness: Brightness.light,
-  ));
+  runApp(MyApp());
+}
 
-  SystemChrome.setEnabledSystemUIOverlays(
-      [SystemUiOverlay.top, SystemUiOverlay.bottom]);
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  runApp(const AppWidget());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
 }
